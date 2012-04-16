120240608c9ae2bb4e2e03865e553238dfc38afb
Rick Copeland <rick@arborian.com>
2012-04-16 09:55:09 -0400

    Initial commit
    Signed-off-by: Rick Copeland <rick@arborian.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='Giza',
+      version=version,
+      description="Some functions I've found useful in creating Pyramid applications",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='rick@arborian.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
