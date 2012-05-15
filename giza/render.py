fad0ef7fca99c67f38203f9090e8d3cc12df626b
Rick Copeland <rick@arborian.com>
2012-05-15 16:22:49 -0400

    Make ew optional
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- giza/render.py
+++ giza/render.py
@@ -7,8 +7,11 @@ import pkg_resources
 from zope.interface import implements
 from pyramid.interfaces import ITemplateRenderer
 
-import ew.render
-from ew import kajiki_ew
+try:
+    import ew.render
+    from ew import kajiki_ew
+except ImportError:
+    ew = None
 
 class JSONRenderer(object):
     def __init__(self, info): 
@@ -38,52 +41,52 @@ class JSONRenderer(object):
             return value.__json__()
         raise TypeError, value
         
+if ew is not None:
+    class EWTemplateRenderer(object):
+        implements(ITemplateRenderer)
 
-class EWTemplateRenderer(object):
-    implements(ITemplateRenderer)
+        def __init__(self, info):
+            pass
 
-    def __init__(self, info):
-        pass
+        @property
+        def resource_manager(self):
+            return ew.widget_context.resource_manager
 
-    @property
-    def resource_manager(self):
-        return ew.widget_context.resource_manager
+        def implementation(self): # pragma no cover
+            return self
 
-    def implementation(self): # pragma no cover
-        return self
+        def __call__(self, value, system):
+            """ ``value`` is the result of the view.
+            Returns a result (a string or unicode object useful as a
+            response body). Values computed by the system are passed in the
+            ``system`` parameter, which is a dictionary containing:
 
-    def __call__(self, value, system):
-        """ ``value`` is the result of the view.
-        Returns a result (a string or unicode object useful as a
-        response body). Values computed by the system are passed in the
-        ``system`` parameter, which is a dictionary containing:
+            * ``view`` (the view callable that returned the value),
+            * ``renderer_name`` (the template name or simple name of the renderer),
+            * ``context`` (the context object passed to the view), and
+            * ``request`` (the request object passed to the view).
+            """
+            template, engine_name = system['renderer_name'].rsplit('.', 1)
+            engine = ew.render.TemplateEngine.get_engine(engine_name)
+            Template = engine.load(template, is_fragment=False)
+            context = dict(system)
+            context.update(renderer=self)
 
-        * ``view`` (the view callable that returned the value),
-        * ``renderer_name`` (the template name or simple name of the renderer),
-        * ``context`` (the context object passed to the view), and
-        * ``request`` (the request object passed to the view).
-        """
-        template, engine_name = system['renderer_name'].rsplit('.', 1)
-        engine = ew.render.TemplateEngine.get_engine(engine_name)
-        Template = engine.load(template, is_fragment=False)
-        context = dict(system)
-        context.update(renderer=self)
-        
-        context.update(value)
-        return engine.render(Template, context)
+            context.update(value)
+            return engine.render(Template, context)
 
-    def register_css(self, href, **kw):
-        self.resource_manager.register(kajiki_ew.CSSLink(href, **kw))
+        def register_css(self, href, **kw):
+            self.resource_manager.register(kajiki_ew.CSSLink(href, **kw))
 
-    def register_js(self, href, **kw):
-        self.resource_manager.register(kajiki_ew.JSLink(href, **kw))
+        def register_js(self, href, **kw):
+            self.resource_manager.register(kajiki_ew.JSLink(href, **kw))
 
-    def emit(self, region):
-        return self.resource_manager.emit(region)
+        def emit(self, region):
+            return self.resource_manager.emit(region)
 
-def enable_ew(config):
-    '''Sets up the Kajiki templating language for the specified
-    file extensions.
-    '''
-    for ep in pkg_resources.iter_entry_points('easy_widgets.engines'):
-        config.add_renderer('.' + ep.name, EWTemplateRenderer)
+    def enable_ew(config):
+        '''Sets up the Kajiki templating language for the specified
+        file extensions.
+        '''
+        for ep in pkg_resources.iter_entry_points('easy_widgets.engines'):
+            config.add_renderer('.' + ep.name, EWTemplateRenderer)
